5b0207b990557a66ab181bb71cba1fef5ce33732
mawi <42104518+DEM0NAssissan7@users.noreply.github.com>
2020-11-06 01:37:25 -0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+echo Installing mawlspack...
+echo
+mkdir ~/.icons
+cp -r icons/Papirus ~/.icons/
+cp -r icons/Papirus-Dark ~/.icons/
+cp -r shell/mawlspack-shell ~/.themes/
+cp -r theme/mawlspack-theme ~/.themes/
+echo
+echo Finished!
